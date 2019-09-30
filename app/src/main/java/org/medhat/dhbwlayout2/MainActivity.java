5d0c926990e0c7014e53eab0a5ff5d87eefabca2
Medhat El-Tawil <us5t@gmx.de>
2019-09-30 02:07:17 +0200

    inital commit
    Signed-off-by: Medhat El-Tawil <us5t@gmx.de>

---
+++ app/src/main/java/org/medhat/dhbwlayout2/MainActivity.java
@@ -0,0 +1,14 @@
+package org.medhat.dhbwlayout2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
